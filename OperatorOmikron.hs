b3c67f9554a3dfc8380503ffc2fcd516faa186b6
Kryštof Pešek (Kof) <krystof.pesek@gmail.com>
2024-09-09 07:37:38 +0200

    Update OperatorOmikron.hs

--- OperatorOmikron.hs
+++ OperatorOmikron.hs
@@ -1,10 +1,10 @@
 import Data.List (subsequences)
 
--- Funkce pro generování všech kombinací operátorů
+
 generateExpressions :: [String] -> [String] -> [String]
 generateExpressions vars ops = [buildExpr vars ops opsCombo | opsCombo <- subsequences ops]
 
--- Funkce pro vytvoření výrazu
+
 buildExpr :: [String] -> [String] -> [String] -> String
 buildExpr vars ops opsCombo = concat $ intercalateOp (head vars) (tail vars) opsCombo
   where
@@ -12,10 +12,10 @@ buildExpr vars ops opsCombo = concat $ intercalateOp (head vars) (tail vars) ops
     intercalateOp x [] _ = [x]
     intercalateOp x (y:ys) (o:os) = (x ++ " " ++ o ++ " " ++ y) : intercalateOp y ys os
 
--- Hlavní funkce pro výpis všech kombinací
+
 main :: IO ()
 main = do
-  let vars = ["a", "b", "g", "d"]
-  let ops = ["+", "-", "*", "/"] -- Seznam operátorů, které chceš použít
+  let vars = ["a", "b", "c", "d"]
+  let ops = ["+", "-", "*", "/"] 
   let expressions = generateExpressions vars ops
   mapM_ putStrLn expressions
